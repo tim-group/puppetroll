41652e62d12f96a6ecc0e4732b5f14256b56fb33
David Ellis <david.ellis@timgroup.com>
2012-10-29 16:10:48 +0000

    adding foreman links (harcoded) to output \n

--- lib/puppetroll/client.rb
+++ lib/puppetroll/client.rb
@@ -55,6 +55,8 @@ class PuppetRoll::Client
     @mc.discover :nodes=>nodes
     statuses = {}
 
+    print "RUNNING LAST_RUN_SUMMARY ON #{nodes} \n"
+
     @mc.last_run_summary.each do |node|
       on_error {
         statuses[node[:sender]] = "failed"
